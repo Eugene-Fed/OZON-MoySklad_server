99df4aa2538affd16ae40f307485819aa2ebdf66
Eugene-Fed <eugene.a.fed@yandex.ru>
2021-09-10 15:36:16 +0300

    Обновлена структура api-keys.json - добавлены поля для авторизации в МойСклад

--- README.md
+++ README.md
@@ -1,11 +1,20 @@
 # OZON-MoySklad_server
 
-#api-key json structure:
-
+# api-key json structure:
 {
-    "client_id": "string",
-    "api_key": "string",
-    "url_domain": "string",
-    "url_command": "string"
+    "api_ozon":
+        {
+        "client_id": "string",
+        "api_key": "string",
+        "url_domain": "string",
+        "url_command": "string"
+        },
+    "api_moysklad":
+        {
+        "client_id": "string",
+        "api_key": "string",
+        "url_domain": "string",
+        "url_command": "string"
+        }
 
 }
--- moysklad_export.py
+++ moysklad_export.py
@@ -4,16 +4,16 @@ import json
 with open('api-keys/api-keys.json') as f:
     templates = json.load(f)
 
-client_id = templates['api_ozon']['client_id']
-api_key = templates['api_ozon']['api_key']
-url_domain = templates['api_ozon']['url_domain']
-url_command = templates['api_ozon']['url_command']
+client_id = templates['client_id']
+api_key = templates['api_key']
+url_domain = templates['url_domain']
+url_command = templates['url_command']
 
 dir_to = 'asc'
-date_from = '2021-07-13T00:00:00+03:00' #добавночные нули минут - обязательны, иначе ошибка
-date_to = '2021-09-05T23:59:59+03:00'
+date_from = '2021-07-13T00:00:00+03'
+date_to = '2021-09-05T23:59:59+03'
 status = 'delivered'
-limit = 2
+limit = 5
 offset = 0
 translit = True
 analytics_data = False
@@ -30,7 +30,6 @@ response = requests.post(url_domain+url_command, headers=headers, json=request_b
 json = response.json()
 
 print(response.status_code)
-#print(json)
 print(json['result'][0]['order_id'])
 print(json['result'][1]['products'][0]['offer_id'])
-#print(response.headers)
+print(response.headers)
