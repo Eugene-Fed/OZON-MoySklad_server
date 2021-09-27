c83c9b8a14b40eb2738e86a41f96afc98ca57e40
Eugene-Fed <eugene.a.fed@yandex.ru>
2021-09-27 22:52:37 +0300

    Разбор получаемых по АПИ данных об организации на необходимые поля: href, id, accountId

--- moysklad_import-meta.py
+++ moysklad_import-meta.py
@@ -8,23 +8,29 @@ api_key = api_params['api_key']
 url_domain = api_params['url_domain']
 url_command = '/api/remap/1.2/entity/organization'
 
-#filter_ = {'since':date_from, 'status':status, 'to':date_to}
-#with_ = {'analytics_data':analytics_data, 'financial_data':financial_data}
+# filter_ = {'since':date_from, 'status':status, 'to':date_to}
+# with_ = {'analytics_data':analytics_data, 'financial_data':financial_data}
 
-headers = {'Authorization':'Bearer '+api_key}
+headers = {'Authorization': 'Bearer '+api_key}
 request_body = {}
 
 response_organization = requests.get(url_domain+'/api/remap/1.2/entity/organization', headers=headers)
 print("Статус запроса Юр.лиц: " + str(response_organization.status_code))
 
-data = response_organization.json()
-format_data = json.dumps(data, indent=4) #красивый вывод в консоль
+response_data = response_organization.json()
+format_data = json.dumps(response_data['rows'][0], indent=4)  # красивый вывод в консоль
 
 with open('meta/meta_ozon_input.json') as f:
     big_data = json.load(f)
-big_data['organization'] = data
+# big_data['organization']['href'] = response_data['rows'][0]['meta']['href']
+# big_data['organization']['id'] = response_data['rows']['id']
+# big_data['organization']['accountId'] = response_data['rows']['accountId']
 
+# 'rows' --> 'meta' --> 'href'
+# 'rows' --> 'id'
+# 'rows' --> 'accountId'
 
 with open('meta/meta_ozon_input.json', 'w') as outfile:
     json.dump(big_data, outfile)
     print(format_data)
+    #print(big_data)
