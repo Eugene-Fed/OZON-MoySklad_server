cf27a9dec6109ec739c50375cba101b1e5061f22
Eugene-Fed <eugene.a.fed@yandex.ru>
2021-10-01 19:22:38 +0300

    В скрипт moysklad_export.py добавлена переменная increase_time которая позволит прибавлять 1 минуту между датами создания каждого следующего архивного заказа. Этоп озволит распоожить все заказы в правильном порядке, согласно реальному времени их создания. Нужно всегда держать в уме то, что дата/время воздания заказа не может быть ранее даты/времени открытия смены. При этом на открытие смены требуется несколько миллисекунд (с момента оправки запроса до момента ее открытия). Как следствие необходимо в дальнейшем будет перестраховаться и прибавлять как минимум 1 секунду ко времени оформления первого заказа на случай.

--- moysklad_export.py
+++ moysklad_export.py
@@ -1,6 +1,6 @@
 import requests
 import json
-from datetime import datetime, date, time
+from datetime import datetime, timedelta, date, time
 
 with open('api-keys/api-keys.json') as f:                       # Получаем параметры для отправки запроса на сервер
     api_params = json.load(f)['api_moysklad']
@@ -9,7 +9,7 @@ api_key = api_params['api_key']                                 # Получае
 api_domain = api_params['api_domain']                           # Получаем домен API
 api_url = api_params['api_url']                                 # Получаем основной путь для работы с API
 
-with open('data/retailShifts.json') as f:                            # Открываем файл с открытыми сменами МойСклад
+with open('data/retailShifts.json') as f:                            # Файл с открытыми сменами МойСклад
     moysklad_retailShifts = json.load(f)
 print('Список открытых смен:')
 print(json.dumps(moysklad_retailShifts, indent=2, ensure_ascii=False))
@@ -22,15 +22,27 @@ with open('data/ozon_orders.json') as f:                        # Открыва
 # Как следствие при выгрузке нам практически не пригодится поле с датой оформления заказа из файла выгрузки ОЗОН
 # Только если мы будем скриптом по расписанию ежесуточно открывать смену в полночь и в дальнейшем в смену
 # на соответствуюущую дату отправлять оформленные заказы
-orders_date = ozon_orders['result'][0]['created_at']        # получаем строку с датой/временем первого заказа из списка
-orders_date = datetime.fromisoformat(orders_date[:-1])      # преобразуем в формата даты/времени, убирая конечный Z
+order_date = ozon_orders['result'][0]['created_at']        # получаем строку с датой/временем первого заказа из списка
+order_date = datetime.fromisoformat(order_date[:-1])      # преобразуем в формата даты/времени, убирая конечный Z
+print('Тип переменной order_date: ' + str(type(order_date)))
+print(order_date)
+
+# Создаем переменную формата ДатаВремя со значением 1 минута для того, чтобы в цикле выгрузки заказов озон
+# в смену МойСклад, каждый следующий заказ приходил на 1 минуту позже, чем предыдущий, начиная с момента открытия смены
+# Эта головомойка нужна временно для выгрузки старых заказов, созданных ранее, т.к. в МойСклад дата оформления заказа
+# Должна быть позже даты открытия смены. При этом невозможно октрыть смену задним числом. По идее, возможно отправить
+# Все заказы с одним значением времени, но дабы избежать возможных проблем сделаем это с зареджкой в 1 минуту
+increase_time = timedelta(minutes=1)
+print('Тип переменной increase_time: ' + str(type(increase_time)))
+print(increase_time)
+new_time = order_date + increase_time
+print(new_time)
 
 # utc3 = datetime.time(3, 0, 0)
 # orders_data = datetime.strftime("%Y-%m-%d %H:%M:%S", orders_data)
 
-print(type(orders_date))
-print(orders_date)
-# print(orders_data.strftime("%Y-%m-%d %H:%M:%S"))
+
+# print(order_data.strftime("%Y-%m-%d %H:%M:%S"))
 
 
 # filter_ = {'since':date_from, 'status':status, 'to':date_to}
