0c274759ab91506c97c97a63cfe2f2d5915670a7
Eugene-Fed <eugene.a.fed@yandex.ru>
2021-10-01 17:50:03 +0300

    Привел переменные для заголовка и тела запроса в moysklad_export.py к формату, общему для всего проекта;переименован скрипт moysklad_retailShift-create.py --> moysklad_retailShifts-open.py добавлены построчные комментарии в moysklad_export.py

--- moysklad_export.py
+++ moysklad_export.py
@@ -1,23 +1,24 @@
 import requests
 import json
-import datetime
 from datetime import datetime, date, time
 
-with open('api-keys/api-keys.json') as f:
+with open('api-keys/api-keys.json') as f:                       # Получаем параметры для отправки запроса на сервер
     api_params = json.load(f)['api_moysklad']
 
-# client_id = api_params['api_ozon']['client_id']
-api_key = api_params['api_key']
-url_domain = api_params['api_domain']
-# url_command = api_params['url_command']
-url_command = '/api/remap/1.2/entity/retaildemand'
-meta_href = '/api/remap/1.2/entity/retailshift'
+api_key = api_params['api_key']                                 # Получаем ключ API MoySklad
+api_domain = api_params['api_domain']                           # Получаем домен API
+api_url = api_params['api_url']                                 # Получаем основной путь для работы с API
 
-with open('data/data.json') as f:
-    orders_data = json.load(f)
+with open('data/ozon_orders.json') as f:                        # Открываем файл с заказами ОЗОН за все время
+    ozon_orders = json.load(f)
 
-orders_date = orders_data['result'][0]['created_at'] # получаем строку с датой временем заказа
-orders_date = datetime.fromisoformat(orders_date[:-1]) # преобразуем в формата даты/времени, убирая конечный Z
+# МойСклад не принимает формат ДатаВремя с конечным Z, поэтому убираем его перед отправкой запроса
+# Надо иметь ввиду, что в МойСклад нельзя отправит заказ с датой раньше, чем дата открытия смены,
+# Как следствие при выгрузке нам практически не пригодится поле с датой оформления заказа из файла выгрузки ОЗОН
+# Только если мы будем скриптом по расписанию ежесуточно открывать смену в полночь и в дальнейшем в смену
+# на соответствуюущую дату отправлять оформленные заказы
+orders_date = ozon_orders['result'][0]['created_at']        # получаем строку с датой/временем первого заказа из списка
+orders_date = datetime.fromisoformat(orders_date[:-1])      # преобразуем в формата даты/времени, убирая конечный Z
 
 # utc3 = datetime.time(3, 0, 0)
 # orders_data = datetime.strftime("%Y-%m-%d %H:%M:%S", orders_data)
@@ -30,13 +31,14 @@ print(orders_date)
 # filter_ = {'since':date_from, 'status':status, 'to':date_to}
 # with_ = {'analytics_data':analytics_data, 'financial_data':financial_data}
 
-# 'Content-Type': 'application/json' - указывать не обязательно, если реквест используется с декодером (request.post(json=dataset))
-headers = {'Authorization':'Bearer '+api_key}
+# 'Content-Type': 'application/json' - указывать не обязательно,
+# если реквест используется с декодером (request.post(json=dataset))
+headers = {'Authorization': 'Bearer ' + api_key}
 # request_body = {'dir':dir_to, 'filter':filter_, 'limit':limit, 'offset':offset,\
 #                    'translit':translit, 'with':with_}
 request_body = {}
 
-response = requests.post(url_domain+url_command, headers=headers, json=request_body)
+response = requests.post(api_domain + api_url + 'retaildemand', headers=headers, json=request_body)
 data = response.json()
 
 print(response.status_code)
--- ozon_import.py
+++ ozon_import.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import datetime
 
 with open('api-keys/api-keys.json') as f:
     api_params = json.load(f)['api_ozon']   # выбираем только данные объекта api_ozon
@@ -10,17 +11,20 @@ api_key = api_params['api_key']
 api_domain = api_params['api_domain']
 api_url = api_params['api_url']
 
-dir_to = 'asc'                          # порядок сортировки: asc - по возрастанию, desc - по убыванию
-date_from = '2021-07-13T00:00:00Z'      # Начальная и конечная дата для выгрузки заказов
-# date_from = '2021-09-30T00:00:00Z'
-date_to = '2021-10-30T23:59:59Z'        # Z = UTF+0 для того, чтобы смена открывалась/закрывалась в 3 ночи по МСК
-# status = 'delivered'                    # delivered - только доставленные/выкупленные заказы *не обязательный параметр
-status = ''
-limit = 1000                               # Макс. количество выгружаемых заказов от 1 до 1000
-offset = 0                              # Кол-во пропускаемых элементов. Если offset = 10, то ответ начнется с 11 заказа
-translit = True                         # True, если включена транслитерация адреса из кириллицы в латиницу
-analytics_data = False                  # Не включать данные аналитики в ответ
-financial_data = True                   # Включить финансовые данные в ответ, чтобы посчитать общую комиссию с продажи
+# Начальная и конечная дата для выгрузки заказов. Текст формата '2021-07-13T00:00:00Z'
+# Z на конце = UTF+0 для того, чтобы смена открывалась в 3 ночи по МСК
+# Смена начинается и заканчивается в 00:00 для того, чтобы избежать потери заказов между 23:59 и 00:00
+date_from = '2021-07-13T00:00:00Z'
+date_today = datetime.date.today().strftime("%Y-%m-%d")     # Получаем текущую дату и преобразуем в текст понятный API
+date_to = date_today + 'T00:00:00Z'
+
+dir_to = 'asc'                      # Порядок сортировки: asc - по возрастанию, desc - по убыванию
+status = ''                         # delivered, cancelled, delivering, awaiting_deliver, awaiting_packaging *не обязат.
+limit = 1000                        # Макс. количество выгружаемых заказов от 1 до 1000
+offset = 0                          # Кол-во пропускаемых элементов. Если offset = 10, то ответ начнется с 11 заказа
+translit = True                     # True, если включена транслитерация адреса из кириллицы в латиницу
+analytics_data = False              # Не включать данные аналитики в ответ
+financial_data = True               # Включить финансовые данные в ответ, чтобы посчитать общую комиссию с продажи
 
 filter_ = {'since': date_from, 'status': status, 'to': date_to}     # Фильтр для поиска отправленийпо параметрам
 with_ = {'analytics_data': analytics_data, 'financial_data': financial_data}    # Доп. поля, для добавления в ответ
@@ -31,10 +35,10 @@ request_body = {'dir': dir_to, 'filter': filter_, 'limit': limit, 'offset': offs
 
 # Отправляем запрос к API ОЗОН для получения списка заказов за указанный ранее период
 response_orders = requests.post(api_domain + api_url, headers=headers, json=request_body)
-print(response_orders.status_code)              # Вывод в консоль старус ответа сервера
+print('Статус запроса: ' + str(response_orders.status_code))                # Вывод в консоль старус ответа сервера
 json_orders = response_orders.json()
 
-format_data = json.dumps(json_orders, indent=2, ensure_ascii=False)  # Преобразуем ответ сервера в JSON
+format_data = json.dumps(json_orders, indent=2, ensure_ascii=False)         # Преобразуем ответ сервера в JSON
 
 print(json.dumps(json_orders, indent=2, ensure_ascii=False))
 print('\nКоличество заказов: ' + str(len(json_orders['result'])))
@@ -42,9 +46,9 @@ print('\nКоличество заказов: ' + str(len(json_orders['result'])
 # print(json_orders['result'][1]['products'][0]['offer_id'])             # Артикул товара в заказе
 # print(response.headers)
 
-for element in json_orders['result']:
-    print(element['status'])
-
-with open('data/ozon_orders.json', 'w') as outfile:
-    json.dump(json_orders, outfile, indent=4, ensure_ascii=False)
+# for element in json_orders['result']:
+#     print(element['status'] + '\t' + element['created_at'])
+#
+# with open('data/ozon_orders.json', 'w') as outfile:
+#     json.dump(json_orders, outfile, indent=4, ensure_ascii=False)
 #      print(format_data)
