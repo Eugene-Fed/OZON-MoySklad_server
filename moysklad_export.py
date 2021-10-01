85b777fc6d5448b31f3617042b4254cb2cfe03b0
Eugene-Fed <eugene.a.fed@yandex.ru>
2021-10-01 20:15:15 +0300

    moysklad_export.py - добавлен цикл, который проходит по всем заказам из файла ozon_orders.py По этому циклу из списка заказов извлекается номер заказа и вычисляется ДатаВремя создания заказа, которые будут экспортированы в Смену МойСклад. Дальше необходимо в этом же цикле получить все остальные необходимые данные о заказе и сформировать соответствующий json-объект. Этот json-объект должен соответствовать API МойСклад Возможно, вместо большого json-объекта с заказами нужно каждый заказ в отдельности и отправлять его в API МойСклад по очереди в том же цикле с задержкой в 10 секунд с помощью sleep.

--- moysklad_export.py
+++ moysklad_export.py
@@ -9,23 +9,16 @@ api_key = api_params['api_key']                                 # Получае
 api_domain = api_params['api_domain']                           # Получаем домен API
 api_url = api_params['api_url']                                 # Получаем основной путь для работы с API
 
-with open('data/retailShifts.json') as f:                            # Файл с открытыми сменами МойСклад
+with open('data/retailShifts.json') as f:                       # Файл с открытыми сменами МойСклад
     moysklad_retailShifts = json.load(f)
+
+retailShift_create_date = moysklad_retailShifts['retailShifts'][0]['created']      # ДатаВремя создания смены в МойСклад
+
 print('Список открытых смен:')
 print(json.dumps(moysklad_retailShifts, indent=2, ensure_ascii=False))
 
 with open('data/ozon_orders.json') as f:                        # Открываем файл с заказами ОЗОН за все время
     ozon_orders = json.load(f)
-
-# МойСклад не принимает формат ДатаВремя с конечным Z, поэтому убираем его перед отправкой запроса
-# Надо иметь ввиду, что в МойСклад нельзя отправит заказ с датой раньше, чем дата открытия смены,
-# Как следствие при выгрузке нам практически не пригодится поле с датой оформления заказа из файла выгрузки ОЗОН
-# Только если мы будем скриптом по расписанию ежесуточно открывать смену в полночь и в дальнейшем в смену
-# на соответствуюущую дату отправлять оформленные заказы
-order_date = ozon_orders['result'][0]['created_at']        # получаем строку с датой/временем первого заказа из списка
-order_date = datetime.fromisoformat(order_date[:-1])      # преобразуем в формата даты/времени, убирая конечный Z
-print('Тип переменной order_date: ' + str(type(order_date)))
-print(order_date)
 
 # Создаем переменную формата ДатаВремя со значением 1 минута для того, чтобы в цикле выгрузки заказов озон
 # в смену МойСклад, каждый следующий заказ приходил на 1 минуту позже, чем предыдущий, начиная с момента открытия смены
@@ -33,10 +26,26 @@ print(order_date)
 # Должна быть позже даты открытия смены. При этом невозможно октрыть смену задним числом. По идее, возможно отправить
 # Все заказы с одним значением времени, но дабы избежать возможных проблем сделаем это с зареджкой в 1 минуту
 increase_time = timedelta(minutes=1)
-print('Тип переменной increase_time: ' + str(type(increase_time)))
-print(increase_time)
-new_time = order_date + increase_time
-print(new_time)
+
+# Здесь будет храниться "виртуальное" время заказа, чтобы заказ попал в Смену
+# order_date = retailShift_create_date                   # Время первого заказа считается от времени открытия смены
+order_date = datetime.strptime(retailShift_create_date, "%Y-%m-%d %H:%M:%S.%f")  # Преобразуем строку в ДатаВремя
+for order in ozon_orders['result']:
+    order_date += increase_time                # Время заказа = +1 минута к созданию смены и созданию предыдущего заказа
+    order_date_export = datetime.strftime(order_date, "%Y-%m-%d %H:%M:%S")
+    print(str(order['order_id']) + ': ' + order_date_export)
+# order_date_export = datetime.fromisoformat(order_date[:-1])  # преобразуем в формата даты/времени, убирая конечный Z
+
+# МойСклад не принимает формат ДатаВремя с конечным Z, поэтому убираем его перед отправкой запроса
+# Надо иметь ввиду, что в МойСклад нельзя отправит заказ с датой раньше, чем дата открытия смены,
+# Как следствие при выгрузке нам практически не пригодится поле с датой оформления заказа из файла выгрузки ОЗОН
+# Только если мы будем скриптом по расписанию ежесуточно открывать смену в полночь и в дальнейшем в смену
+# на соответствуюущую дату отправлять оформленные заказы
+# order_date = ozon_orders['result'][0]['created_at'] # получаем строку с датой/временем первого заказа из списка
+# order_date = datetime.fromisoformat(order_date[:-1])     # преобразуем в формата даты/времени, убирая конечный Z
+# print('Тип переменной order_date: ' + str(type(order_date)))
+# print(order_date)
+
 
 # utc3 = datetime.time(3, 0, 0)
 # orders_data = datetime.strftime("%Y-%m-%d %H:%M:%S", orders_data)
